587d6aa574e99f6d08bf6bcd8f497465923b5548
HadiqaAziz <hadiqa.aziz@gmail.com>
2023-08-07 17:58:55 -0700

    Delete score-board.js

--- score-board.js
+++
@@ -1,8 +0,0 @@
-const highestScoresListEl = document.getElementById("highScoresList");
-const highestScoresEl = JSON.parse(localStorage.getItem("highScores")) || [];
-
-highestScoresListEl.innerHTML = highestScoresEl
-  .map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;
-  })
-  .join("");
